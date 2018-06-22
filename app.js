a7fbd245e845aa012f918d08265553f97106f505
jenn14108 <jenn.lee14108@gmail.com>
2018-06-22 11:47:21 -0400

    Add user authentication functionality

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const
   userInfoController = require('./controllers/userInfoController');
   searchHistoryController = require('./controllers/searchHistoryController');
   mongoose = require( 'mongoose');
-
   mainPageRouter = require('./routes/mainPage');
   aboutRouter = require('./routes/about');
-//searchRouter = require('./routes/search');
-  //signUpRouter = require('./routes/signUp');
+  //Set up needed variables in order to do authentication
+  //GoogleStrategy = require('passport-google-oauth').OAuth25Strategy; --> in cofig/passport.js
+  session = require('express-session');
+  passport = require('passport');
+  configPassport = require('./config/passport');
+  configPassport(passport);
 
 var app = express();
 
@@ -28,6 +31,10 @@ db.once('open', function() {
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+//CODE FOR AUTHENTICATION
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -39,6 +46,37 @@ app.use('/', mainPageRouter);
 app.use('/about', aboutRouter);
 //app.use('/search', searchRouter);
 //app.use('/signUp', signUpRouter);
+
+
+//Authentication routes
+app.get('/loginerror', function(req,res){
+  res.render('loginerror',{})
+})
+app.get('/login', function(req,res){
+  res.render('login',{})
+})
+// route for logging out
+app.get('/logout', function(req, res) {
+        req.logout();
+        res.redirect('/');
+    });
+// =====================================
+// GOOGLE ROUTES =======================
+// =====================================
+// send to google to do the authentication
+// profile gets us their basic information including their name
+// email gets their emails
+//This rout is visited to start the google authentication. Passport will send you to
+//Google to get authenticated. Then, it will send the browser back to /login/authorized page
+app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+
+app.get('/login/authorized',
+        passport.authenticate('google', {
+                successRedirect : '/',
+                failureRedirect : '/loginerror'
+        }));
+
 
 //routing for the page that stores user information into the database
 app.get('/signUp', userInfoController.getAllUserInfo);
