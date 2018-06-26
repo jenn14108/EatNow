afb7543a22ef34b1feefdd363a518404b6410d82
jenn14108 <jenn.lee14108@gmail.com>
2018-06-26 15:03:38 -0400

    Change the callback URL

--- config/auth.js
+++ config/auth.js
@@ -4,6 +4,6 @@ module.exports = {
     'googleAuth' : {
         'clientID': '979302746758-kfh1l60io9vtpu10jtlf0flnn2r77g1k.apps.googleusercontent.com',
         'clientSecret'  : 'g4fQ0ls0_cVlzcGt6Chuh2xX',
-        'callbackURL'   : 'http://127.0.0.1:5700/login/authorized'
+        'callbackURL'   : 'http://leiner.cs-i.brandeis.edu:5700/login/authorized'
     }
 };
