dd20c6d49a288778436f8855419b6b1c98d2174e
jenn14108 <jenn.lee14108@gmail.com>
2018-06-26 14:09:48 -0400

    Change port number

--- config/auth.js
+++ config/auth.js
@@ -2,9 +2,9 @@ module.exports = {
 
 
     'googleAuth' : {
-        'clientID': '246096591118-ti33uv184e4m1bib9grgn8alm45btadb.apps.googleusercontent.com',
-        'clientSecret'  : 'iqgLqu6pXgLuHsZFq6nvxDX3',
-        'callbackURL'   : 'http://127.0.0.1:5000/login/authorized'
+        'clientID': '979302746758-kfh1l60io9vtpu10jtlf0flnn2r77g1k.apps.googleusercontent.com',
+        'clientSecret'  : 'g4fQ0ls0_cVlzcGt6Chuh2xX',
+        'callbackURL'   : 'http://127.0.0.1:5700/login/authorized'
     }
 
 };
