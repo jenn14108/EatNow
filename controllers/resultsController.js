ae3ab6ab23ec5ef9b53583bfb38d16d1bc7afb2c
jenn14108 <jenn.lee14108@gmail.com>
2018-08-12 15:12:36 -0400

    Add conditionals for picking new suggestions

--- controllers/resultsController.js
+++ controllers/resultsController.js
@@ -5,16 +5,22 @@ const yelp = require('yelp-fusion');
 const yelpClientID = process.env.YELPCLIENTID;
 const yelpAPIKey = process.env.YELPAPIKEY;
 const bodyParser = require('body-parser');
-var restaurantName;
-var restaurantLocation;
-var yelpLink;
-var googleQuery;
+var restaurantName; var restaurantLocation;
+var yelpLink; var googleQuery;
 var allRestaurants; var resultLength;
+var pickedNums = [];
+var ranOut = false;
 
 exports.renderMain = (req, res) => {
-  res.render('Results', {title: "Results", name:restaurantName,
-                        location: restaurantLocation, yelpLink:yelpLink,
-                        googleQuery: googleQuery});
+  if (ranOut == true) {
+    var response = "Unfortunately, we ran out of suggestions :(";
+    res.render('Results', {title:"Results", name:response,
+                            ranOut: ranOut});
+  } else {
+    res.render('Results', {title: "Results", name:restaurantName,
+                          location: restaurantLocation, yelpLink:yelpLink,
+                          googleQuery: googleQuery, ranOut: ranOut});
+  }
 };
 
 exports.yelpFindRestaurant = (req,res,next) => {
@@ -30,6 +36,11 @@ exports.yelpFindRestaurant = (req,res,next) => {
     .then(response => {
       resultLength = response.jsonBody.businesses.length;
       var pickedNum = Math.floor(Math.random() * (resultLength - 0 + 1)) + 0;
+      pickedNums.push(pickedNum);
+      console.log("LENGTH");
+      console.log(resultLength);
+      console.log("FIRST PICKED NUMBER:");
+      console.log(pickedNum);
       const pickedRestaurant = response.jsonBody.businesses[pickedNum];
       //console.log(pickedRestaurant);
       //const prettyJson = JSON.stringify(firstResult, null, 4)
@@ -49,20 +60,42 @@ exports.yelpFindRestaurant = (req,res,next) => {
     .catch(err => {
       console.log(err);
   });
-}
+};
 
 exports.findAnotherRestaurant = (req,res,next) => {
   console.log('finding another restaurant...');
-  console.log(resultLength)
-  var pickedNum = Math.floor(Math.random() * (resultLength - 0 + 1)) + 0;
-  console.log(pickedNum);
-  const pickedRestaurant = allRestaurants[pickedNum];
-  console.log(pickedRestaurant);
-  restaurantName = pickedRestaurant.name;
-  restaurantLocation = pickedRestaurant.location.display_address[0] + "\n" +
-                        pickedRestaurant.location.display_address[1];
-  yelpLink = pickedRestaurant.url;
-  googleQuery = restaurantName.replace(/\s/g, "+") + "+"
-                + restaurantLocation.replace(/\s/g, "+");
-  next();
-}
+  if (pickedNums.length == resultLength){
+    ranOut = true;
+    next();
+  } else {
+    // console.log("THIS IS THE LENGTH: ");
+    // console.log(resultLength);
+    var pickedNum;
+    var picked = true;
+    while (picked) {
+      picked = false;
+      pickedNum = Math.floor(Math.random() * ((resultLength-1) - 0 + 1)) + 0;
+      for (var i = 0; i < pickedNums.length; i++){
+        if ((pickedNum == pickedNums[i])){
+          picked = true;
+        }
+      }
+      if (picked) {
+        pickedNum = Math.floor(Math.random() * (resultLength - 0 + 1)) + 0;
+      } else {
+        pickedNums.push(pickedNum);
+      }
+    }
+    // console.log("FINAL PICK: ");
+    // console.log(pickedNum);
+    const pickedRestaurant = allRestaurants[pickedNum];
+    //console.log(pickedRestaurant);
+    restaurantName = pickedRestaurant.name;
+    restaurantLocation = pickedRestaurant.location.display_address[0] + "\n" +
+                          pickedRestaurant.location.display_address[1];
+    yelpLink = pickedRestaurant.url;
+    googleQuery = restaurantName.replace(/\s/g, "+") + "+"
+                  + restaurantLocation.replace(/\s/g, "+");
+    next();
+  };
+};
